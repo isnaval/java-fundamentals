ac9e75534579712c312e4bf5a22bf5f042650f61
IsmaelNV <ismaelnavarrovalencia@gmail.com>
2025-05-31 18:44:52 +0200

    Actualización ejercicios básicos

---
+++ foundations/java_core_basic/basic_algorithms/DigitReverser.java
@@ -0,0 +1,13 @@
+package java_core_basic.basic_algorithms;
+
+public class DigitReverser {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println("La suma de los 10 primeros numeros naturales es de: " + sum);
+	}
+
+}
---
+++ foundations/java_core_basic/basic_algorithms/PowerCalculator.java
@@ -0,0 +1,5 @@
+package java_core_basic.basic_algorithms;
+
+public class PowerCalculator {
+
+}
